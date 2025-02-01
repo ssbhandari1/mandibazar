b13cc8e7c7d27efd4c65e70a260371d5b374ad23
santosh <santoshbirkhal@gmail.com>
2025-02-01 15:43:53 +0530

    fix database

--- src/server/lib/db.ts
+++ src/server/lib/db.ts
@@ -11,20 +11,14 @@ export const connectToDB = async () => {
   try {
     const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
     const options: ConnectOptions = {
-      // You can add additional options here if needed, like:
-      // user: 'yourUsername',
-      // pass: 'yourPassword',
       dbName: 'mandi',
     };
 
 
     const connection = await mongoose.connect(dbUri, options);
     isConnected = true;
-
-    console.log('MongoDB connected');
     return connection;
   } catch (error) {
-    console.error('MongoDB connection error:', error);
     throw new Error('Failed to connect to MongoDB');
   }
 };
